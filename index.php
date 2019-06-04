78d0be940c86d78caa65c97c60c53cc8e9f148af
Edilson do Nascimento <edilsonjoper@gmail.com>
2019-06-04 14:43:56 -0300

    02 - Efetuando o login e cookies

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <?php include("cabecalho.php");
 
- 	if(isset($_GET["login"]) && $_GET["login"]==="true"){?>  
-		<p class="alert-success">Logado com sucesso!</p>
-	<?php } 
-	if(isset($_GET["login"]) && $_GET["login"]==="false"){?>
-		<p class="alert-danger">Usuário ou senha!</p>
-	<?php } ?>
-
-<h2>Login</h2>
-<form action="login.php" method="post">
-	  <div class="form-group">
-	  		<label>Email address</label>
-	    	<input type="email" class="form-control" name="email" placeholder="Enter email">
-	  </div>
-	  <div class="form-group">
-	    	<label>Password</label>
-	    	<input type="password" class="form-control" name="senha" placeholder="Password">
-	  </div>	 
-	  <button type="submit" class="btn btn-primary">Logar</button>
-</form>
+	if(isset($_COOKIE['usuario_logado'])) { ?>
+		<p class="text-success">Vocẽ está logado como <?=$_COOKIE["usuario_logado"]?></p>
+	<?php }else{ ?>
+		<h2>Login</h2>
+		<form action="login.php" method="post">
+			  <div class="form-group">
+			  		<label>Email address</label>
+			    	<input type="email" class="form-control" name="email" placeholder="Enter email">
+			  </div>
+			  <div class="form-group">
+			    	<label>Password</label>
+			    	<input type="password" class="form-control" name="senha" placeholder="Password">
+			  </div>	 
+			  <button type="submit" class="btn btn-primary">Logar</button>
+		</form>
+	<?php } ?>  
 
 <?php include("rodape.php");?>
--- login.php
+++ login.php
@@ -4,7 +4,9 @@
 	 $email = $_POST['email'];
 	 $senha = $_POST['senha'];
 
+
 	 if($usuario = buscaUsuario($conexao, $email, $senha)){
+	 	setcookie("usuario_logado", $usuario["email"], time() + 30);
 	 	header("Location: index.php?login=true");
 	 }else{
 	 	header("Location: index.php?login=false");
